f41ab36fd51086b2bf7ee6fb349a74fcf0b3af0c
petermagaga <ptrmagaga@gmail.com>
2026-04-16 10:00:42 +0300

    models

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from data_ingestion.models import Dataset
 
-# Create your models here.
+class AnalysisResult(models.Model):
+    dataset=models.OneToOneField(Dataset,on_delete=models.CASCADE)
+    summary=models.JSONField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f" analysis for {self.dataset.name}"
--- insights/models.py
+++ insights/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from data_ingestion.models import Dataset
 
-# Create your models here.
+
+class Insight(models.Model):
+    dataset=models.ForeignKey(Dataset,on_delete=models.CASCADE)
+    contents=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Insight for {self.dataset.name}"
+
