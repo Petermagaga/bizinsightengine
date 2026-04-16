26e6fc83affe1350c1e708453ac760df0f3d783e
petermagaga <ptrmagaga@gmail.com>
2026-04-16 08:56:44 +0300

    superuser

--- config/settings.py
+++ config/settings.py
@@ -5,15 +5,10 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+AUTH_USER_MODEL ='users.User'
+
 DEBUG = os.getenv('DEBUG') == "True"
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(",")
@@ -109,7 +104,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
