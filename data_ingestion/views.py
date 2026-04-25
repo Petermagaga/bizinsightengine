6f0351312321ff62dfed9d5147b5b84c2df42810
petermagaga <ptrmagaga@gmail.com>
2026-04-25 04:58:56 +0300

    Upload → Parse → Store → Analyze → AI → Save → Background Worker

--- data_ingestion/views.py
+++ data_ingestion/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from .models import Dataset,DataRecord
 from .serializers import DatasetSerializer
 from .utils import parse_excel
+from analytics.tasks import process_dataset_task
+
 
 @api_view(['GET','POST'])
 def upload_dataset(request):
@@ -13,7 +15,8 @@ def upload_dataset(request):
 
     if serializer.is_valid():
         dataset = serializer.save(user=request.user if request.user.is_authenticated else None)
-
+        process_dataset_task.delay(dataset.id)
+        
         try:
             parsed_data =parse_excel(dataset.file)
 
