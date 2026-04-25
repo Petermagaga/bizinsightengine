eae772c45effd4afd3000f296626acafde278a48
petermagaga <ptrmagaga@gmail.com>
2026-04-25 05:33:14 +0300

    Upload → Parse → Store → Analyze → AI → Save → Background Worker

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .serializers import RegisterSerializer
+from core
