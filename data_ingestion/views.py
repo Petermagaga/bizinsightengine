8170e9070fdb078f3a2dbeacae21a0b3e64bae0f
petermagaga <ptrmagaga@gmail.com>
2026-04-27 00:59:46 +0300

    authentication

--- data_ingestion/views.py
+++ data_ingestion/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 from .models import Dataset,DataRecord
 from .serializers import DatasetSerializer
 from .utils import parse_excel
 from analytics.tasks import process_dataset_task
 
 
-@api_view(['GET','POST'])
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def upload_dataset(request):
     serializer=DatasetSerializer(data=request.data)
 
@@ -16,7 +19,7 @@ def upload_dataset(request):
     if serializer.is_valid():
         dataset = serializer.save(user=request.user if request.user.is_authenticated else None)
         process_dataset_task.delay(dataset.id)
-        
+
         try:
             parsed_data =parse_excel(dataset.file)
 
--- insights/views.py
+++ insights/views.py
@@ -1,12 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+
 from .models import Insight
 from .serializers import InsightSerializer
 from data_ingestion.models import Dataset
 
 
+
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_insights(request,dataset_id):
     try:
         dataset=Dataset.objects.get(id=dataset_id)
