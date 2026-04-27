baea26eb5926a0a3323949ed8e4e8d1d21bcd69e
petermagaga <ptrmagaga@gmail.com>
2026-04-28 00:23:19 +0300

    views.data

--- data_ingestion/serializers.py
+++ data_ingestion/serializers.py
@@ -3,11 +3,16 @@ from .models import Dataset
 
 
 class DatasetSerializer(serializers.ModelSerializer):
+    uploaded_at = serializers.DateTimeField(read_only=True)
+
     class Meta:
-        model=Dataset
-        fields =["id","name","file","uploaded_at"]
-        read_only_fields= ["id",'uploaded_at']
-    def validate_file(self,value):
-        if not value.name.endswith(('.xlsx','.xls')):
-            raise serializers.ValidationError("Only Excel files are allowed (.xlsx,xls)")
+        model = Dataset
+        fields = ["id", "name", "file", "uploaded_at"]
+        read_only_fields = ["id", "uploaded_at"]
+
+    def validate_file(self, value):
+        if not value.name.endswith(('.xlsx', '.xls')):
+            raise serializers.ValidationError(
+                "Only Excel files are allowed (.xlsx, .xls)"
+            )
         return value
