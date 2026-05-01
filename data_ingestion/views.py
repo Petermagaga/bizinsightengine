95c9d3bdec0d86bd47909de319da8f7626393ba2
petermagaga <ptrmagaga@gmail.com>
2026-05-01 06:45:14 +0300

    views.py data_ingestion

--- data_ingestion/views.py
+++ data_ingestion/views.py
@@ -2,45 +2,10 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from django.db import transaction
 
-import json
-from .models import Dataset, DataRecord
 from .serializers import DatasetSerializer
-from .utils import parse_excel
 from analytics.tasks import process_dataset_task
 
-
-import math
-from datetime import datetime, date
-from decimal import Decimal
-
-def clean_row(row):
-    cleaned = {}
-
-    for k, v in row.items():
-
-        # Handle datetime
-        if isinstance(v, (datetime, date)):
-            cleaned[k] = v.isoformat()
-
-        # Handle Decimal
-        elif isinstance(v, Decimal):
-            cleaned[k] = float(v)
-
-        # Handle NaN / Infinity
-        elif isinstance(v, float) and (math.isnan(v) or math.isinf(v)):
-            cleaned[k] = None
-
-        # Handle normal JSON-safe types
-        elif isinstance(v, (int, float, str, bool)) or v is None:
-            cleaned[k] = v
-
-        # Fallback (VERY important)
-        else:
-            cleaned[k] = str(v)
-
-    return cleaned
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -50,29 +15,14 @@ def upload_dataset(request):
     if not serializer.is_valid():
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    try:
-        with transaction.atomic():
-            # Save dataset
-            dataset = serializer.save(user=request.user)
+    dataset = serializer.save(user=request.user)
 
-            # Parse file
-            parsed_data = parse_excel(dataset.file)
+    process_dataset_task.delay(dataset.id)
 
-            # Save records safely
-            records = []
-            for row in parsed_data:
-                clean_data = clean_row(row)
-                records.append(DataRecord(dataset=dataset, data=clean_data))
-
-            DataRecord.objects.bulk_create(records)
-
-            # Trigger processing AFTER records exist
-            process_dataset_task.delay(dataset.id)
-
-    except Exception as e:
-        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-    # Re-serialize saved instance
-    response_data = DatasetSerializer(dataset).data
-
-    return Response(response_data, status=status.HTTP_201_CREATED)
+    return Response(
+        {
+            "message": "Dataset uploaded successfully. Processing started.",
+            "dataset_id": dataset.id
+        },
+        status=status.HTTP_201_CREATED
+    )
