820b616b0370e0aa02c438bb411f10296eb25e8b
petermagaga <ptrmagaga@gmail.com>
2026-05-03 01:14:41 +0300

    task

--- data_ingestion/urls.py
+++ data_ingestion/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import upload_dataset
-
+from .views import upload_dataset, dataset_status
 
 urlpatterns = [
-    path('upload/',upload_dataset)
+    path("upload/", upload_dataset),
+    path("<int:dataset_id>/status/", dataset_status),
 ]
--- data_ingestion/views.py
+++ data_ingestion/views.py
@@ -3,6 +3,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Dataset
+
+
 from .serializers import DatasetSerializer
 from analytics.tasks import process_dataset_task
 
@@ -26,3 +34,22 @@ def upload_dataset(request):
         },
         status=status.HTTP_201_CREATED
     )
+
+
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def dataset_status(request, dataset_id):
+    try:
+        dataset = Dataset.objects.get(id=dataset_id, user=request.user)
+    except Dataset.DoesNotExist:
+        return Response({"error": "Not found"}, status=404)
+
+    return Response({
+        "id": dataset.id,
+        "name": dataset.name,
+        "status": dataset.status,
+        "progress": getattr(dataset, "progress", 0),
+        "uploaded_at": dataset.uploaded_at,
+    })
