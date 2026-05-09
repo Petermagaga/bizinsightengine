0699abc703c5e2cac07892b9ab24963b16df360e
petermagaga <ptrmagaga@gmail.com>
2026-05-10 02:53:53 +0300

    analytics.GET_FAILED_ROWS

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,6 +1,7 @@
-from .views import get_analysis
+from .views import get_analysis,FailedRow
 from django.urls import path
 
 urlpatterns = [
     path("<int: dataset_id>/",get_analysis),
+    path("<int:dataset_id>/failed",FailedRow)
 ]
