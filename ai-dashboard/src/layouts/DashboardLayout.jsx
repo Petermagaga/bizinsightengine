d8e2bca985397b0e6e6d4a6d37e93d13e10a3cfd
petermagaga <ptrmagaga@gmail.com>
2026-05-10 03:22:08 +0300

    Frontend

---
+++ ai-dashboard/src/layouts/DashboardLayout.jsx
@@ -0,0 +1,22 @@
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="flex bg-gray-100 min-h-screen">
+
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col">
+
+        <Navbar />
+
+        <main className="p-6">
+          {children}
+        </main>
+
+      </div>
+
+    </div>
+  );
+}
