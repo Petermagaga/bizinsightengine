241f741659255c9de65a5f64aa501dbdfc18b54d
petermagaga <ptrmagaga@gmail.com>
2026-05-18 17:36:27 +0300

    dashboard  analytics

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,8 +1,13 @@
-from .views import get_analysis,get_failed_rows,dataset_status
+from .views import (get_analysis,get_failed_rows,
+                    dataset_status,dashboard_data)
 from django.urls import path
 
 urlpatterns = [
     path("<int:dataset_id>/analysis/",get_analysis),
     path("<int:dataset_id>/failed/",get_failed_rows),
     path("<int:dataset_id>/status/",dataset_status),
+    path(
+        "<int:dataset_id>/dashboard/",
+        dashboard_data
+    )
 ]
