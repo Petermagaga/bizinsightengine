8517428447f90c3ee092f5bb0965557602910f42
petermagaga <ptrmagaga@gmail.com>
2026-05-19 22:51:23 +0300

    trends_service.py

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,6 +1,7 @@
 from .views import (get_analysis,get_failed_rows,
                     dataset_status,dashboard_data)
 from django.urls import path
+from trends_service import get_trends
 
 urlpatterns = [
     path("<int:dataset_id>/analysis/",get_analysis),
@@ -9,5 +10,6 @@ urlpatterns = [
     path(
         "<int:dataset_id>/dashboard/",
         dashboard_data
-    )
+    ),
+    path("<int:dataset_id>/trends/",get_trends)
 ]
