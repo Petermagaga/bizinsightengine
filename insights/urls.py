f79648c7cae39a137fe9a9ca1a5c4e5135c39ab8
petermagaga <ptrmagaga@gmail.com>
2026-05-21 12:18:50 +0300

    urls

--- insights/urls.py
+++ insights/urls.py
@@ -1,7 +1,21 @@
+
 from django.urls import path
-from .views import get_insights,dashboard_data
+
+from .views import (
+    get_insights,
+    dashboard_data
+)
 
 urlpatterns = [
-    path("<int:dataset_id>/",get_insights),
-    path("<int:dataset_id>/dashboard/",dashboard_data),
+    path(
+        "<int:dataset_id>/",
+        get_insights,
+        name="get_insights"
+    ),
+
+    path(
+        "dashboard/<int:dataset_id>/",
+        dashboard_data,
+        name="dashboard_data"
+    ),
 ]
