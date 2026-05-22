1a7355d309139c3afff4e246103821ec0f9c24d4
petermagaga <ptrmagaga@gmail.com>
2026-05-22 14:51:24 +0300

    api configurations

--- frontend/src/api/dashboardApi.js
+++ frontend/src/api/dashboardApi.js
@@ -5,6 +5,23 @@ const API = axios.create({
     "http://127.0.0.1:8000/api/v1/",
 });
 
+API.interceptors.request.use(
+  (config) => {
+
+    const token =
+      localStorage.getItem(
+        "access_token"
+      );
+
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  }
+);
+
 export const getDashboard =
   async (datasetId) => {
 
