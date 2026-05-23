3caf86e0d0f6eec3aa99940030842c3ecc250baa
petermagaga <ptrmagaga@gmail.com>
2026-05-23 10:50:02 +0300

    _token refresh

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/token/",
          TokenObtainPairView.as_view(),name="token_obtain_pair"),
+    path("api/token/refresh/",TokenRefreshView.as_view(),name="token_refresh"),
     path('api/test/',test_api),
     path('api/data/',include('data_ingestion.urls')),
     path('api/v1/insights/',include('insights.urls')),
