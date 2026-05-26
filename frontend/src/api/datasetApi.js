1da80fc6edb623e18b03a6f7ecd20b86e7f2996f
petermagaga <ptrmagaga@gmail.com>
2026-05-26 12:11:13 +0300

    dataset

---
+++ frontend/src/api/datasetApi.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL:
+    "http://127.0.0.1:8000/api/data/",
+});
+
+API.interceptors.request.use(
+  (config) => {
+
+    const token =
+      localStorage.getItem(
+        "access_token"
+      );
+
+    if (token) {
+
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  }
+);
+
+export const getDatasets =
+  async () => {
+
+    const response =
+      await API.get(
+        "datasets/"
+      );
+
+    return response.data;
+  };
