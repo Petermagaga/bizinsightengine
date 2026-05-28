8c687b9bdd7d3971fcbf269b2cd0a5035db7fc7d
petermagaga <ptrmagaga@gmail.com>
2026-05-28 12:58:21 +0300

    refactor- Insights

--- insights/models.py
+++ insights/models.py
@@ -1,19 +1,7 @@
 from django.db import models
 from data_ingestion.models import Dataset
 
-class Meta: 
-    ordering=["-created_at "]
-    
 class Insight(models.Model):
 
-    dataset=models.ForeignKey(Dataset,on_delete=models.CASCADE)
-    summary_text=models.TextField(null=True,blank=True)
-    bi_insights=models.JSONField(default=dict,blank=True)
-    predictions=models.JSONField(default=dict,blank=True)
-    created_at=models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Insight for {self.dataset.name} at {self.created_at} "
-
-
+    dataset=models
 
